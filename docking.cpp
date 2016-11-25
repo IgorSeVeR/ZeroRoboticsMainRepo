201069948a69545de7e2430e59d1ee57a48dd368
IgorSeVeR <egorov_is@inbox.ru>
2016-11-26 00:12:33 +0300

    Update docking.cpp

--- docking.cpp
+++ docking.cpp
@@ -10,11 +10,8 @@ float dock_dis[6];
 void init()
 {
     dock_dis[0] = 0.173;
-	dock_dis[1] = 0.173;
-	dock_dis[2] = 0.160;
-	dock_dis[3] = 0.160;
-	dock_dis[4] = 0.146;
-	dock_dis[5] = 0.146;
+	dock_dis[1] = 0.160;
+	dock_dis[2] = 0.146;
 } 
 
 float getDis(float* pos1, float* pos2) 
@@ -40,7 +37,7 @@ void takeCube(int num)
 	  mathVecNormalize(item_att,3);
     float length = mathVecMagnitude(item_att,3);
     for(int i = 0; i<3; ++i)
-        loc_tofly[i] = item_att[i]*dock_dis[num]/length + item_loc[i]; //i can tell u how it works
+        loc_tofly[i] = item_att[i]*dock_dis[num / 2]/length + item_loc[i]; //i can tell u how it works
     api.setPositionTarget(loc_tofly);
     api.setAttitudeTarget(point_att);
     api.getMyZRState(myState);
@@ -51,7 +48,7 @@ void takeCube(int num)
     }
     api.setPositionTarget(loc_tofly);
     api.setAttitudeTarget(point_att);
-    if ((mathVecMagnitude(myVel,3) < 0.01) && (getDis(myState,item_loc)<=dock_dis[num]) && (game.isFacingCorrectItemSide(num)) && (!(game.hasItem(num)))) game.dockItem(num);
+    if ((mathVecMagnitude(myVel,3) < 0.01) && (getDis(myState,item_loc)<=dock_dis[num / 2]) && (game.isFacingCorrectItemSide(num)) && (!(game.hasItem(num)))) game.dockItem(num);
 }
 
 void loop()
